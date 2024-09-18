8ce4db7229a4c240cd71e8fbeabd8bdf3f54adde
Yaroslava670 <Yasya_santa@mail.ru>
2024-09-18 16:50:20 +0300

    Add files via upload

---
+++ pythonProject5/ьфшт.py
@@ -0,0 +1,5 @@
+home_work_all = '12'
+all_time_hour = '1.5'
+name_course = 'Python'
+time_work = '0.125'
+print('Курс:',name_course,'всего задач:',home_work_all,'затрачено часов:',all_time_hour,'среднее время выполнения 0.125 часа')
